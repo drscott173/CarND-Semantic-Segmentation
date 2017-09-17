fcad868e88759d55b708396ca952ecb537e03fea
Scott Penberthy <scott.penberthy@gmail.com>
2017-09-17 05:08:02 -0700

    first working version with augmentation

--- helper.py
+++ helper.py
@@ -2,6 +2,7 @@ import re
 import random
 import numpy as np
 import os.path
+import scipy
 import scipy.misc
 import shutil
 import zipfile
@@ -10,6 +11,7 @@ import tensorflow as tf
 from glob import glob
 from urllib.request import urlretrieve
 from tqdm import tqdm
+import cv2
 
 
 class DLProgress(tqdm):
@@ -84,14 +86,19 @@ def gen_batch_function(data_folder, image_shape):
             for image_file in image_paths[batch_i:batch_i+batch_size]:
                 gt_image_file = label_paths[os.path.basename(image_file)]
 
-                image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
-                gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+                image = cv2.resize(cv2.imread(image_file).astype('uint8'), image_shape[::-1])
+                gt_image = cv2.resize(cv2.imread(gt_image_file).astype('uint8'), image_shape[::-1])
 
-                gt_bg = np.all(gt_image == background_color, axis=2)
+                image_a, gt_image_a = augment(image, gt_image)
+
+                #image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+                #gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+
+                gt_bg = np.all(gt_image_a == background_color, axis=2)
                 gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
                 gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
 
-                images.append(image)
+                images.append(image_a)
                 gt_images.append(gt_image)
 
             yield np.array(images), np.array(gt_images)
@@ -138,3 +145,122 @@ def save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_p
         sess, logits, keep_prob, input_image, os.path.join(data_dir, 'data_road/testing'), image_shape)
     for name, image in image_outputs:
         scipy.misc.imsave(os.path.join(output_dir, name), image)
+
+def get_pair(data_folder="./data/data_road/training", image_shape=(160, 576)):
+    image_paths = glob(os.path.join(data_folder, 'image_2', '*.png'))
+    label_paths = {
+        re.sub(r'_(lane|road)_', '_', os.path.basename(path)): path
+        for path in glob(os.path.join(data_folder, 'gt_image_2', '*_road_*.png'))}
+    background_color = np.array([255, 0, 0])
+
+    image_path = random.choice(image_paths)
+    print(image_path)
+    label_path = label_paths[os.path.basename(image_path)]
+    image = cv2.resize(cv2.imread(image_path).astype('uint8'), image_shape[::-1])
+    label_img = cv2.resize(cv2.imread(label_path).astype('uint8'), image_shape[::-1])
+    return image, label_img
+
+##
+## Image augmentation
+##
+
+def bgr2rgb(img):
+    img2 = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    return img2
+
+def bgr2hls(img):
+    img2 = cv2.cvtColor(img,cv2.COLOR_BGR2HLS)
+    return img2
+
+def hls2bgr(img):
+    img1 = cv2.cvtColor(img, cv2.COLOR_HLS2BGR)
+    return img1
+
+def bgr2hsv(img):
+    img1 = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    return img1
+
+def hsv2bgr(img):
+    img1 = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+    return img1
+
+def add_random_shadow(image):
+    #
+    # Add a random shadow to a BGR image to pretend
+    # we've got clouds or other interference on the road.
+    #
+    rows,cols,_ = image.shape
+    top_y = cols*np.random.uniform()
+    top_x = 0
+    bot_x = rows
+    bot_y = cols*np.random.uniform()
+    image_hls = bgr2hls(image)
+    shadow_mask = 0*image_hls[:,:,1]
+    X_m = np.mgrid[0:image.shape[0],0:image.shape[1]][0]
+    Y_m = np.mgrid[0:image.shape[0],0:image.shape[1]][1]
+    shadow_mask[((X_m-top_x)*(bot_y-top_y)-(bot_x - top_x)*(Y_m-top_y) >=0)] = 1
+    random_bright = .25+.7*np.random.uniform()
+    if (np.random.randint(2) ==1 ):
+        random_bright = .5
+        cond1 = (shadow_mask==1)
+        cond0 = (shadow_mask==0)
+        if np.random.randint(2)==1:
+            image_hls[:,:,1][cond1] = image_hls[:,:,1][cond1]*random_bright
+        else:
+            image_hls[:,:,1][cond0] = image_hls[:,:,1][cond0]*random_bright
+    image = hls2bgr(image_hls)
+    return image
+
+def augment_brightness_camera_images(image):
+    #
+    # expects input image as BGR, adjusts brightness to
+    # pretend we're in different lighting conditions.
+    #
+    image1 = bgr2hsv(image)
+    random_bright = .25+np.random.uniform()
+    image1[:,:,2] = image1[:,:,2]*random_bright
+    image2 = hsv2bgr(image1)
+    return image2
+
+def trans_image(img,label,trans_range):
+    #
+    # Shift image up or down a bit within trans_range pixels,
+    # filling missing area with black.  IMG is in BGR format.
+    #
+    rows, cols, _ = img.shape
+    tr_x = trans_range*np.random.uniform()-trans_range/2
+    tr_y = 10*np.random.uniform()-10/2
+    #print("Image shape is ", rows, "x", cols, " shifting ", tr_x, " in x ", tr_y, " in y")
+    #tr_y = 0
+    Trans_M = np.float32([[1,0,tr_x],[0,1,tr_y]])
+    img_tr = cv2.warpAffine(img,Trans_M,(cols,rows))
+    label_tr = cv2.warpAffine(label,Trans_M,(cols,rows))
+    return img_tr, label_tr
+
+def augment(img_raw, label):
+    #
+    # We "augment" images during training.
+    #
+    # imageList is a list of camera image pathnames [left, center, right]
+    # y is the label for these three images
+    #
+    # We choose one of the camera images, augment it to simulate
+    # potential environments, then return a chosen image and label y.
+    #
+    img = augment_brightness_camera_images(img_raw)
+    if (np.random.randint(4) == 0):
+        img_shadows = add_random_shadow(img)
+    else:
+        img_shadows = img
+    if (np.random.randint(2) == 0):
+        img_trans, label_trans = trans_image(img_shadows, label, 25)
+    else:
+        img_trans, label_trans = img_shadows, label
+    if (np.random.randint(4) == 0):
+        img_flip = cv2.flip(img_trans, 1)
+        label_flip = cv2.flip(label_trans, 1)
+    else:
+        img_flip, label_flip = img_trans, label_trans
+    img_rgb = bgr2rgb(img_flip)
+    label_rgb = bgr2rgb(label_flip)
+    return img_rgb, label_rgb
--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import helper
 import warnings
 from distutils.version import LooseVersion
 import project_tests as tests
-
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
 
 # Check TensorFlow Version
 assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
@@ -15,7 +18,6 @@ if not tf.test.gpu_device_name():
     warnings.warn('No GPU found. Please use a GPU to train your neural network.')
 else:
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
-
 
 def load_vgg(sess, vgg_path):
     """
@@ -32,10 +34,18 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
-tests.test_load_vgg(load_vgg, tf)
 
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+
+    graph = tf.get_default_graph()
+    image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    l3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    l4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    l7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    return image_input, keep, l3, l4, l7
+
+#tests.test_load_vgg(load_vgg, tf)
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
@@ -45,10 +55,35 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param vgg_layer3_out: TF Tensor for VGG Layer 7 output
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
-    """
+
     # TODO: Implement function
-    return None
-tests.test_layers(layers)
+    """
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, strides=(1,1),
+        padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, strides=(2,2), padding='same',
+                                        kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    # add in skip connections from the first pooling layer
+    skip = tf.layers.conv2d_transpose(vgg_layer4_out, num_classes, 1, padding='same',
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.add(output, skip)
+    output = tf.layers.conv2d_transpose(output, num_classes, 4, strides=(2,2), padding='same',
+                                        kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    # add in skip connections from the second pooling layer
+    skip = tf.layers.conv2d_transpose(vgg_layer3_out, num_classes, 1, padding='same',
+                                      kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                      kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.add(output, skip)
+    output = tf.layers.conv2d_transpose(output, num_classes, 16, strides=(8,8), padding='same',
+                                        kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    return output
+
+#tests.test_layers(layers)
 
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
@@ -61,12 +96,24 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
-tests.test_optimize(optimize)
+
+    # flatten the image have 2 classes per pixel
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    correct_label = tf.reshape(correct_label, (-1, num_classes))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits,
+                                                                                labels=correct_label))
+
+    # Add the optimizer
+    train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
+
+#tests.test_optimize(optimize)
 
 
-def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
-             correct_label, keep_prob, learning_rate):
+def train_nn(sess, epochs, batch_size, get_batches_fn,
+             train_op, cross_entropy_loss,
+             input_image, correct_label, keep_prob, learning_rate):
     """
     Train neural network and print out the loss during training.
     :param sess: TF Session
@@ -80,12 +127,27 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
-tests.test_train_nn(train_nn)
 
+    losses = []
+    sess.run(tf.global_variables_initializer())
 
-def run():
+    for epoch in range(epochs):
+        print("EPOCH {} ...".format(epoch + 1))
+        for image, label in get_batches_fn(batch_size):
+            feed_dict = {input_image: image,
+                         correct_label: label,
+                         keep_prob: 0.5,
+                         learning_rate: 1.0e-3}
+            _, loss_value = sess.run([train_op, cross_entropy_loss],
+                                     feed_dict=feed_dict)
+            losses.append(loss_value)
+            print("Loss: = {:.3f}".format(loss_value))
+        print()
+    return losses
+
+#tests.test_train_nn(train_nn)
+
+def run(epochs=5, batch_size=4):
     num_classes = 2
     image_shape = (160, 576)
     data_dir = './data'
@@ -108,12 +170,29 @@ def run():
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
-        # TODO: Build NN using load_vgg, layers, and optimize function
+        # Build NN using load_vgg, layers, and optimize function
+        input_image, keep_prob, l3, l4, l7 = load_vgg(sess, vgg_path)
+        nn_last_layer = layers(l3, l4, l7, num_classes)
+        correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
+        learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+        logits, train_op, cross_entropy_loss = optimize(nn_last_layer, correct_label, learning_rate, num_classes)
 
-        # TODO: Train NN using the train_nn function
+        # Train NN using the train_nn function
+        train_losses = train_nn(sess,
+                                # hyper parameters
+                                epochs, batch_size, get_batches_fn,
 
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+                                # sgd operator and loss function
+                                train_op, cross_entropy_loss,
+
+                                # placeholders
+                                input_image, correct_label, keep_prob, learning_rate)
+
+
+        # Save inference data using helper.save_inference_samples
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+
+        return train_losses
 
         # OPTIONAL: Apply the trained model to a video
 
